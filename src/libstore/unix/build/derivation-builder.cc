94016602a8d5328c9e750165ac4aa3619a49c395
Nahum Shalman <nshalman@edgecast.io>
2025-07-10 17:58:08 +0000

    Add cfmakeraw implementation for illumos/Solaris
    cfmakeraw is a BSD-specific function not available on illumos.
    Provide a standard POSIX implementation that achieves the same
    terminal raw mode functionality.

--- src/libstore/unix/build/derivation-builder.cc
+++ src/libstore/unix/build/derivation-builder.cc
@@ -41,6 +41,20 @@
 
 #include "store-config-private.hh"
 
+#ifdef __sun
+// cfmakeraw is not available on illumos/Solaris, provide implementation
+static void cfmakeraw(struct termios *termios_p) {
+    termios_p->c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP
+                           | INLCR | IGNCR | ICRNL | IXON);
+    termios_p->c_oflag &= ~OPOST;
+    termios_p->c_lflag &= ~(ECHO | ECHONL | ICANON | ISIG | IEXTEN);
+    termios_p->c_cflag &= ~(CSIZE | PARENB);
+    termios_p->c_cflag |= CS8;
+    termios_p->c_cc[VMIN] = 1;
+    termios_p->c_cc[VTIME] = 0;
+}
+#endif
+
 namespace nix {
 
 MakeError(NotDeterministic, BuildError);
